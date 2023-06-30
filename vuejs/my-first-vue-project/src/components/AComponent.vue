739b646e180d86f2d9c4056e79e1f4ff3b87a1d5
AllanBernier <allan.bernier1@gmail.com>
2023-06-30 17:33:00 +0200

    Vue Js

---
+++ vuejs/my-first-vue-project/src/components/AComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Mon component {{ name }}</h1>
+
+    <h1 v-if="usera"> Mon component {{ usera.email}}</h1>
+    <h1 v-else> no name set</h1>
+
+</template>
+
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps(['name', 'usera'])
+
+</script>
+
+
+<style>
+
+</style>
---
+++ vuejs/my-first-vue-project/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>About Page</h1>
+
+
+    <a-component :name="toto"/>
+    <a-component name="lolo" :usera="user"/>
+
+</template>
+
+
+<script setup>
+import AComponent from './../components/AComponent.vue'
+
+let toto = "tata"
+
+
+let user = {
+    name: "lolo",
+    email: "lolo@gmail.com"
+}
+
+</script>
