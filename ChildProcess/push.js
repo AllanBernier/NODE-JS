574756155823635021eba9facad9e4de0959065a
AllanBernier <allan.bernier1@gmail.com>
2023-06-30 15:39:41 +0200

    Test Push Js

--- ChildProcess/push.js
+++ ChildProcess/push.js
@@ -2,38 +2,8 @@ const {exec} = require('child_process')
 let args = process.argv.slice(2)
 
 exec('git add *',(error, stdout, stderr) => {
-
-    if (error){
-        console.log("Error :" + error)
-        return
-    }
-    if (stderr){
-        console.log("Stderr :" + stderr)
-        return
-    }
-    console.log(stdout)
-})
-exec('git commit -m "'+ args[0] +'"',(error, stdout, stderr) => {
-
-    if (error){
-        console.log("Error :" + error)
-        return
-    }
-    if (stderr){
-        console.log("Stderr :" + stderr)
-        return
-    }
-    console.log(stdout)
-})
-exec('git push',(error, stdout, stderr) => {
-
-    if (error){
-        console.log("Error :" + error)
-        return
-    }
-    if (stderr){
-        console.log("Stderr :" + stderr)
-        return 
-    }
-    console.log(stdout)
+    exec('git commit -m "'+ args[0] +'"',(error, stdout, stderr) => {
+        exec('git push')
+    })
+    
 })
